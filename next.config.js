9c1fda9f6062ca990065ccdfbb335e8c1e4f2413
claudiaazambuja <cclabdis@gmail.com>
2025-10-14 18:57:37 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const isGH = process.env.GITHUB_PAGES === 'true' // opcional
+
+const repo = 'FlowGuilt-frontend' // <--- troque
+
+module.exports = {
+  output: 'export',            // gera HTML estático (out/)
+  images: { unoptimized: true }, // GH Pages não suporta Image Optimization
+  trailingSlash: true,         // ajuda com 404/refresh
+  basePath: `/${repo}`,        // caminhos sob /REPO
+  assetPrefix: `/${repo}/`,    // assets com prefixo /REPO/
+}
